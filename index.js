f2ff0d2277a39700fc12f566ef2cd93c30875e7b
SibaPrasadTech <spc014@hotmail.com>
2022-07-29 21:05:49 +0530

    Express application setup with Basic Auth Routes

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => { console.log("DB Connection successfull!") })
+  .catch((error) => {
+    console.log(error)
+  })
+
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/user', userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Express sever is running in backend at port ${5000}`);
+})
