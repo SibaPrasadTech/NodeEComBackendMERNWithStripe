28e6bfd1f5c7d897639e7c9b1684cf607d2b1034
SibaPrasadTech <spc014@hotmail.com>
2022-08-01 19:27:38 +0530

    Order Product Cart Stripe Routes Added

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -15,6 +18,9 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/carts', cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Express sever is running in backend at port ${5000}`);
